2e2cb14ab8a0a32f911c10ce31b503c9736e0987
Tharindu97 <tharindukavishna@gmail.com>
2021-07-04 22:06:16 +0530

    synchronise and Asynchronise

---
+++ index.js
@@ -0,0 +1,9 @@
+function timeFunction() {
+    console.log("Start Time Out");
+    setTimeout(function ads() {
+        console.log("Tharindu");
+    }, 5000);
+    console.log("All End");
+}
+
+timeFunction();
